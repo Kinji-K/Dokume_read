bc7910dbd1df8ca3bf3f7ec746075c84150a21ff
Kinji_Kamizaki <Kinji.Kamizaki@gmail.com>
2021-01-23 07:02:28 +0000

    slack post

--- __main__.py
+++ __main__.py
@@ -1,5 +1,12 @@
 ﻿from DokumeRead import DokumeRead
 from WriteWiki import WriteWiki
+from Slack import SlackPost
 
-DokumeRead()
-WriteWiki()
+def main():
+    DokumeRead()
+    WriteWiki()
+    slack = SlackPost()
+    slack.WebhookSlack("wikiの更新が完了しました")
+
+if __name__ == "__main__":
+    main()
